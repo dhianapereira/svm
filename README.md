a04320918621bcdb1167752ee5550f3a5a760f7b
Dhiana <dhianaslv@outlook.com>
2021-03-17 16:14:08 -0300

    Atualizaçao no README

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ SVM de um conjunto de dados de câncer de mama que gera uma probabilidade e cate
 
 ## Dados para o treinamento
 
-São 500 pacientes no total, sendo 262 (52,4%) com casos de tumores benignos e 238 (47,6%) com casos de tumores malignos. Para o treinamento foram utilizados 80% dos dados, sendo 40% de casos de tumores benignos e 40% de tumores malignos, e para o teste os 20% restante. Destes 20%, 12,4% são de tumores benignos e 7,6% são de tumores malignos.
+O modelo foi construído com a utilização de um conjunto de dados de 699 pacientes com câncer de mama. O conjunto de dados passou por normalização e limpeza, o que resultou em 500 pacientes no conjunto de dados finais para treinamento e teste.
+
+São 500 pacientes no total, sendo 262 (52,4%) com casos de tumores benignos e 238 (47,6%) com casos de tumores malignos. Para o treinamento foram utilizados 80% dos dados, sendo 40% de casos de tumores benignos e 40% de tumores malignos, e para o teste os 20% restantes. Destes 20%, 12,4% são de tumores benignos e 7,6% são de tumores malignos.
 
 ## Como executar
 
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,30 +1,27 @@
 function loadData() {
-  var data = [];
-
   $.getJSON("../src/data/breast-cancer.json", function (json) {
-    data = json;
+    var data = json;
+
+    var columns = [
+      "clumpThickness",
+      "uniformityOfCellSize",
+      "uniformityOfCellShape",
+      "marginalAdhesion",
+      "singleEpithelialCellSize",
+      "bareNuclei",
+      "blandChromatin",
+      "normalNucleoli",
+      "mitoses",
+    ];
+
+    var result = data.map(function (obj) {
+      return columns.map(function (key) {
+        return obj[key];
+      });
+    });
 
     $.getJSON("../src/data/labels.json", function (json) {
-      var labels = [];
       var labels = json;
-
-      var columns = [
-        "clumpThickness",
-        "uniformityOfCellSize",
-        "uniformityOfCellShape",
-        "marginalAdhesion",
-        "singleEpithelialCellSize",
-        "bareNuclei",
-        "blandChromatin",
-        "normalNucleoli",
-        "mitoses",
-      ];
-
-      var result = data.map(function (obj) {
-        return columns.map(function (key) {
-          return obj[key];
-        });
-      });
 
       const training = result.splice(0, 400);
       console.log("Dados para o treinamento: ");
@@ -36,7 +33,6 @@ function loadData() {
 
       const trainingLabels = labels.splice(0, 400);
 
-      //var wb; // weights and offset structure
       var svm = new svmjs.SVM();
       var svmC = 1.0;
 
@@ -45,26 +41,8 @@ function loadData() {
         C: svmC,
       });
 
-      /*if (console) {
-        // print weights and offset
-        wb = svm.getWeights();
-        for (var i = 0; i < wb.w.length; i++) {
-          console.log("w_%d = %f", i, wb.w[i]);
-        }
-        console.log("b = %f", wb.b);
-        N = training.length;
-        // print predicted margins
-        var marg = svm.margins(data);
-        for (var i = 0; i < N; i++) {
-          console.log("%f, %f", labels[i], marg[i]);
-        }
-      }*/
-
       testLabels = svm.predict(test);
       margins = svm.margins(test);
-
-      //console.log(testLabels);
-      //console.log(margins);
 
       let counter = 0;
       for (let i = 0; i < labels.length; i++) {
